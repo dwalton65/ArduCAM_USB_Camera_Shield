a7ff9f942fd5a8db9fcb4e4cfb33db42ad77c277
David Walton <dwaltoneng@gmail.com>
2023-05-04 17:31:40 +1000

    Use Utils from ArduCAM_USB_Camera_Shield_Cpp_Demo

---
+++ RaspberryPi/Cpp/External_trigger_demo/Utils.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef _ARDUCAM_UTILS_H_
+#define _ARDUCAM_UTILS_H_
+#include <iostream>
+
+#ifdef __linux__
+#include <ArduCamLib.h>
+#endif
+
+#if defined(_WIN32) || defined(_WIN64)
+#include "ArduCamlib.h"
+#endif
+
+#include <opencv2/opencv.hpp>
+
+bool camera_initFromFile(std::string filename, ArduCamHandle &cameraHandle, ArduCamCfg &cameraCfg, int &color_mode, int index = 0);
+cv::Mat ConvertImage(ArduCamOutData* frameData, int color_mode);
+cv::Mat UnpackRaw10(ArduCamOutData* frameData, int drop_row = 0);
+#endif // !
